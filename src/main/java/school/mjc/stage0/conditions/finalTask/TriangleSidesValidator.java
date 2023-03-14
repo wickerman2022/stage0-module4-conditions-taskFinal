2795e6b2e0055a0b4ad6e3f455556c66a70871e0
wickerman2019 <sverige@i.ua>
2023-03-14 22:08:25 +0200

    Fix triangle validation

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,12 +2,30 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if (firstSide*firstSide == secondSide*secondSide + thirdSide*thirdSide) 
-        System.out.println("this is a valid triangle"); else
-        if (secondSide*secondSide == firstSide*firstSide + thirdSide*thirdSide)
-        System.out.println("this is a valid triangle"); else
-        if (thirdSide*thirdSide == firstSide*firstSide + secondSide*secondSide)
-        System.out.println("this is a valid triangle");
-        else System.out.println("it's not a triangle");
+        if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) {
+            System.out.println("it's not a triangle");
+            return;
+        } else if (firstSide + secondSide <= thirdSide || firstSide + thirdSide <= secondSide || secondSide + thirdSide <= firstSide) {
+            System.out.println("it's not a triangle");
+            return;
+        } else if (firstSide == secondSide && secondSide == thirdSide) {
+            System.out.println("this is a valid triangle");
+            return;
+        } else if (firstSide == secondSide || secondSide == thirdSide || firstSide == thirdSide) {
+            System.out.println("this is a valid triangle");
+            return;
+        } else if (firstSide*firstSide == secondSide*secondSide + thirdSide*thirdSide) {
+            System.out.println("this is a valid triangle");
+            return;
+        } else if (secondSide*secondSide == firstSide*firstSide + thirdSide*thirdSide) {
+            System.out.println("this is a valid triangle");
+            return;
+        } else if (thirdSide*thirdSide == firstSide*firstSide + secondSide*secondSide) {
+            System.out.println("this is a valid triangle");
+            return;
+        } else {
+            System.out.println("it's not a triangle");
+            return;
+        }
     }
 }
